b708524172ae239f81182e8c6aab23cf41aca954
Ersoy Tan <ersoy.tanav@gmail.com>
2021-01-19 00:03:28 +0300

    test

---
+++ Product.cs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Product.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.UrunAdi = "kulaklık";
+            urun1.Fiyat = 150;
+            urun1.Ozellik = "air pods";
+
+            Product urun2 = new Product
+            {
+                UrunAdi = "monitor",
+                Fiyat = 750,
+                Ozellik = "gaming"
+            };
+
+
+            Product urun3 = new Product();
+            urun3.UrunAdi = "klavye";
+            urun3.Fiyat = 70;
+            urun3.Ozellik = "laser";
+
+            Product[] products = new Product[] { urun1, urun2, urun3 };
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(products[i].UrunAdi);
+                Console.WriteLine(products[i].Fiyat);
+                Console.WriteLine(products[i].Ozellik);
+                Console.WriteLine(new String('-', 20));
+
+            }
+
+        }
+        class Product
+        {
+            public string UrunAdi { get; set; }
+            public int Fiyat { get; set; }
+            public string Ozellik { get; set; }
+        }
+    }
+}
---
+++ eticaretBasic/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace eticaretBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.UrunAdi = "Araba Kokusu";
+            product1.MagazaAdi = "Glade";
+            product1.Fiyat = 12;
+            product1.Kargo = "Ücretli";
+
+            Product product2 = new Product();
+            product2.UrunAdi = "Pasta Cila";
+            product2.MagazaAdi = "Valet Pro";
+            product2.Fiyat = 123;
+            product2.Kargo = "Ücretsiz";
+
+
+            Product[] products = new Product[] { product1, product2 };
+
+            foreach (var product in products)
+            {
+                Console.WriteLine(product.MagazaAdi + ":" + product.UrunAdi);
+            }
+
+            Console.WriteLine("-------------------");
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine("Ürün Adı: " + products[i].UrunAdi);
+                Console.WriteLine("Mağaza Adı: " + products[i].MagazaAdi);
+                Console.WriteLine("Fiyatı : " + products[i].Fiyat + " TL");
+                Console.WriteLine("Kargo : " + products[i].Kargo);
+                Console.WriteLine("-----------------");
+            }
+
+            int x = 0;
+            while (x < products.Length)
+            {
+                Console.WriteLine(products[x].UrunAdi);
+                Console.WriteLine(products[x].MagazaAdi);
+                Console.WriteLine(products[x].Fiyat);
+                Console.WriteLine(products[x].Kargo);
+                x++;
+            }
+        }
+        class Product
+        {
+            public string UrunAdi { get; set; }
+            public string MagazaAdi { get; set; }
+            public int Fiyat { get; set; }
+            public string Kargo { get; set; }
+
+        }
